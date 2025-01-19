ed73ee8dc626fa05c18277f47a5ecb22ecfed565
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2025-01-19 09:22:58 -0500

    changes settings

--- core/settings.py
+++ core/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -25,12 +27,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="django-insecure-m)jx6lhy_0tuc
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = "RENDER" not in os.environ
+DEBUG = bool( os.environ.get('DEBUG', True) )
 
 ALLOWED_HOSTS = ["*"]
-RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
