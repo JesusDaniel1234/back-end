d379f4936d914e58104dcd3d8862ade538ae9044
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2026-03-18 17:04:34 -0400

    initial commit with develop branch

--- base/views.py
+++ base/views.py
@@ -24,17 +24,15 @@ from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+
 def valordefinidoPorPuntuacion(test, puntuacion):
     print(test, puntuacion)
     valorPorPuntuacion = {
-        "mchatr": "AR" if puntuacion > 7 else "MR" if puntuacion > 2 else "BR" if puntuacion
-        > 1  else "NR",
-        "qchat": "AR" if puntuacion > 51.8 else "MR" if puntuacion > 26.7 else "BR" if puntuacion
-        > 1  else "NR",
-        "qchat10": "AR" if puntuacion > 3 else "BR" if puntuacion
-        > 1  else "NR",
+        "mchatr": "AR" if puntuacion > 7 else "MR" if puntuacion > 2 else "BR" if puntuacion > 1 else "NR",
+        "qchat": "AR" if puntuacion > 51.8 else "MR" if puntuacion > 26.7 else "BR" if puntuacion > 1 else "NR",
+        "qchat10": "AR" if puntuacion > 3 else "BR" if puntuacion > 1 else "NR",
     }
-    
+
     return valorPorPuntuacion[test]
 
 
@@ -53,13 +51,15 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         return token
 
+
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
 
 # base
 class ListarTipoRiesgoView(ListAPIView):
     queryset = TipoRiesgo.objects.all()
-    serializer_class = TipoRiesgoSerializers  
+    serializer_class = TipoRiesgoSerializers
 
 
 class ListarRangoRiesgoView(ListAPIView):
@@ -71,18 +71,18 @@ class ListarValorRiesgoView(ListAPIView):
     queryset = ValorRiesgo.objects.all()
     serializer_class = ValorRiesgoSerializers
 
-def calcularValorRiesgo(mchatr,qchat,qchat10):
+
+def calcularValorRiesgo(mchatr: str, qchat: str, qchat10: str):
     resultados = [mchatr, qchat, qchat10]
-    # Lógica de prioridad
+
     if "AR" in resultados:
-        return "AR"  # Si al menos uno es AR
+        return "AR"
     elif "MR" in resultados:
-        return "MR"  # Si al menos uno es MR (y ninguno es AR)
+        return "MR"
     elif all(r == "BR" for r in resultados):
-        return "BR"  # Si todos son BR
-    
-calcularValorRiesgo("BR","BR","BR")
-    
+        return "BR"
+
+
 def calcularRiesgo(paciente):
     # Usar agregaciones para calcular suma y conteo en una sola consulta
     qchat10_agg = paciente.respuestasqchat10_set.aggregate(
@@ -99,20 +99,22 @@ def calcularRiesgo(paciente):
     respuesta_qchat10 = qchat10_agg['suma'] / qchat10_agg['count'] if qchat10_agg['count'] > 0 else 0
     respuesta_qchat = qchat_agg['suma'] / qchat_agg['count'] if qchat_agg['count'] > 0 else 0
     respuesta_mchatr = mchatr_agg['suma'] / mchatr_agg['count'] if mchatr_agg['count'] > 0 else 0
-    print("Estras son las respuestas:",respuesta_qchat10, respuesta_qchat, respuesta_mchatr)
+    print("Estras son las respuestas:", respuesta_qchat10, respuesta_qchat, respuesta_mchatr)
     # Calcular el riesgo
-    lista_resultados = [0 if respuesta_mchatr < 3 else 1, 0 if respuesta_qchat10 < 3 else 1, 0 if respuesta_qchat < 51 else 1]
-    riesgo = int((sum(lista_resultados)/(len(lista_resultados)))*100)
+    lista_resultados = [0 if respuesta_mchatr < 3 else 1, 0 if respuesta_qchat10 < 3 else 1,
+                        0 if respuesta_qchat < 51 else 1]
+    riesgo = int((sum(lista_resultados) / (len(lista_resultados))) * 100)
 
-    valor_qchat10 = valordefinidoPorPuntuacion("qchat10", respuesta_qchat10 )
-    valor_qchat = valordefinidoPorPuntuacion("qchat", respuesta_qchat )
-    valor_mchatr = valordefinidoPorPuntuacion("mchatr", respuesta_mchatr )
-    
+    valor_qchat10 = valordefinidoPorPuntuacion("qchat10", respuesta_qchat10)
+    valor_qchat = valordefinidoPorPuntuacion("qchat", respuesta_qchat)
+    valor_mchatr = valordefinidoPorPuntuacion("mchatr", respuesta_mchatr)
+
     riesgo = calcularValorRiesgo(valor_qchat10,
-valor_qchat,
-valor_mchatr)
-    
-    return [valor_qchat10,valor_qchat ,valor_mchatr , riesgo]
+                                 valor_qchat,
+                                 valor_mchatr)
+
+    return [valor_qchat10, valor_qchat, valor_mchatr, riesgo]
+
 
 class ListaResultadosGeneralesPorPaciente(APIView):
     def get(self, request):
@@ -134,9 +136,10 @@ class ListaResultadosGeneralesPorPaciente(APIView):
                 "id": paciente.id
             }
             resultado.append(datos_paciente)
-        
+
         return Response(resultado, status=status.HTTP_200_OK)
+
 
 class ServidorActivoView(APIView):
     def get(self, request):
-        return Response({"message":"OK"}, status=status.HTTP_200_OK)
+        return Response({ "message": "OK" }, status=status.HTTP_200_OK)
--- core/settings.py
+++ core/settings.py
@@ -103,23 +103,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "USER": "postgres",
-        "PASSWORD": "APykBBltEkNmLiWUmlLYEjviSyVKiqrU",
-        "HOST": "postgres.railway.internal",
-        "PORT": "5432",
-    }
-}
-# SQLite
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "railway",
+#         "USER": "postgres",
+#         "PASSWORD": "APykBBltEkNmLiWUmlLYEjviSyVKiqrU",
+#         "HOST": "postgres.railway.internal",
+#         "PORT": "5432",
 #     }
 # }
+# SQLite
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # PostgreSQL
 # DATABASES = {
