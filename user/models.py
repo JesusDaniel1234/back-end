aea94f4bf1e79994cde490d44308e035837f5ae2
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2026-03-18 17:17:55 -0400

    modify user models.py

--- user/models.py
+++ user/models.py
@@ -5,34 +5,37 @@ from django.dispatch import receiver
 
 
 # Create your models here.
-def perfil_directorio_ruta(instance, filename):
+def dir_image_user(instance, filename):
+    # Configuración de ruta para almacenar la imagen de perfil de usuario
     return "user_image_{0}/{1}".format(instance.usuario.username, filename)
 
 
-# Create your models here.
-class PerfilUsuario(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    imagen_perfil = models.ImageField(
-        default="user_defoult.PNG", upload_to=perfil_directorio_ruta
+class UserProfile(models.Model):
+    """
+        Modelo de usuario basado en el usuario por defecto de Django
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        default="user_defoult.PNG", upload_to=dir_image_user
     )
 
     def __str__(self):
-        return self.usuario.username
+        return self.user.username
 
 
-# Signals
+# Signals para la gestion del modelo User (Modelo propio de Django)
 @receiver(post_save, sender=User)
-def crear_usuario_perfil(sender, instance, created, **kwargs):
+def create_user(sender, instance, created, **kwargs):
     if created:
-        PerfilUsuario.objects.create(usuario=instance)
+        UserProfile.objects.create(usuario=instance)
 
 
 @receiver(post_save, sender=User)
-def guardar_usuario_perfil(sender, instance, **kwargs):
+def save_user(sender, instance, **kwargs):
     instance.perfilusuario.save()
 
 
-@receiver(post_delete, sender=PerfilUsuario)
-def eliminar_usuario_perfil(sender, instance, **kwargs):
+@receiver(post_delete, sender=UserProfile)
+def delete_user(sender, instance, **kwargs):
     user = instance.usuario
     user.delete()
