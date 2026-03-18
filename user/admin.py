f0dd2e5e1c8fa276fcb647f4f15040ef55261fb6
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2026-03-18 17:32:39 -0400

    add admin configuration in user app

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import PerfilUsuario
+from .models import UserProfile
+
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user__username", "user__first_name", "user__last_name", "user__email"]
 
-admin.site.register(PerfilUsuario)
+
+admin.site.register(UserProfile, UserProfileAdmin)
