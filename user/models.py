a18f9e6759032e681762404c1a110940fa67f4c2
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2026-03-18 17:54:09 -0400

    delete signals

--- user/models.py
+++ user/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-
 
 # Create your models here.
 def dir_image_user(instance, filename):
     # Configuración de ruta para almacenar la imagen de perfil de usuario
-    return "user_image_{0}/{1}".format(instance.usuario.username, filename)
+    return "user_image_{0}/{1}".format(instance.user.username, filename)
 
 
 class UserProfile(models.Model):
@@ -20,21 +17,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-
-# Signals para la gestion del modelo User (Modelo propio de Django)
-@receiver(post_save, sender=User)
-def create_user(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(usuario=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user(sender, instance, **kwargs):
-    instance.perfilusuario.save()
-
-
-@receiver(post_delete, sender=UserProfile)
-def delete_user(sender, instance, **kwargs):
-    user = instance.usuario
-    user.delete()
