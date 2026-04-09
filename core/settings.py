d10654bdc200fdb37eb60c4dd8596b09cce8d3be
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2026-04-09 14:50:37 -0400

    change to local database

--- core/settings.py
+++ core/settings.py
@@ -112,36 +112,24 @@ AUTH_USER_MODEL = 'user.UserProfile'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "USER": "postgres",
-        "PASSWORD": "APykBBltEkNmLiWUmlLYEjviSyVKiqrU",
-        "HOST": "postgres.railway.internal",
-        "PORT": "5432",
-    }
-}
-
-# # SQLite
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "railway",
+#         "USER": "postgres",
+#         "PASSWORD": "APykBBltEkNmLiWUmlLYEjviSyVKiqrU",
+#         "HOST": "postgres.railway.internal",
+#         "PORT": "5432",
 #     }
 # }
 
-# PostgreSQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'evaluatea',
-#         'USER': 'postgres',
-#         'PASSWORD': 'JESUS.daniel1234',
-#         'HOST': 'localhost',
-#         'PORT': '5432',       
-#     }
-# }
+# SQLite
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
