8668e008180e02048820ecb6591f349dd975651f
Jesús Daniel <jesusdanielsanchezalarcon79@gmail.com>
2026-04-09 09:27:25 -0400

    fix user signal

--- apps/user/signals.py
+++ apps/user/signals.py
@@ -1,9 +1,12 @@
 from django.db.models.signals import post_migrate
 from .models import UserProfile
 from django.dispatch import receiver
-
+from django.db import OperationalError, ProgrammingError
 
 @receiver(post_migrate)
 def create_admin_user(sender, **kwargs):
-    if not UserProfile.objects.filter(username="admin").exists():
-        UserProfile.objects.create_superuser(username="admin",email="admin@admin.com", password="admin1234")
+    try:
+        if not UserProfile.objects.filter(username="admin").exists():
+            UserProfile.objects.create_superuser(username="admin", email="admin@admin.com", password="admin1234")
+    except (OperationalError, ProgrammingError):
+        pass
